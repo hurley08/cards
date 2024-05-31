e80e86bb84416641708c0163d573217f44d6aee3
hurley08 <intriqet@gmail.com>
2024-05-31 13:00:56 -0700

    verified working

--- cards/practice.py
+++ cards/practice.py
@@ -1,17 +1,33 @@
 # practice.py
  
 from cards.deck import Deck
+from cards.deck import standard_deck
 from cards.playing_card import PlayingCard, suits, faces
-from cards import cardExceptions
-
+from cardExceptions import *
 
 from dataclasses import dataclass
 from typing import List
 import random
+
+
+
+def generate_std_deck(suits, faces):
+	std_deck = []
+	for i in suits:
+		for j in faces:
+			tup =f"('{i}', '{j}')"
+			std_deck.append(tup)
+			print(tup,',')
+	return std_deck
+
+
 
 d = Deck()
 d.generate_deck()
 print(d.deck)
 print(d.binned())
 print(d.count())
-
+L = d.binned()
+for i in L:
+	print(i)
+	print(L[i], '\n')
