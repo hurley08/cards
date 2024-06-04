5d43b722778ed6d7121a7a04e4b9679249e1129e
hurley08 <intriqet@gmail.com>
2024-06-04 12:49:26 -0700

    Began implementing face_up functionality which itself works but not all methods have been updated to acknowledge this attribute

--- cards/playing_card.py
+++ cards/playing_card.py
@@ -2,64 +2,44 @@
 
 from dataclasses import dataclass
 from cards import cardExceptions
+import logging
 
-
-
-
-suits = {
- 'Club': 0,
- 'Diamond': 2,
- 'Heart': 4,
- 'Spade': 6,
-}
-
-faces = {
- 'Two': 2,
- 'Three': 3,
- 'Four': 4,
- 'Five': 5,
- 'Six': 6,
- 'Seven': 7,
- 'Eight': 8,
- 'Nine': 9,
- 'Ten':10,
- 'Jack': 11,
- 'Queen': 12,
- 'King': 13,
-  'Ace': 14,
-}
+logger = logging.getLogger(__name__)
 
 @dataclass
 class PlayingCard:
     def __init__(self, suit, face, face_up=False, in_hand=False, in_deck=None):
-        self._suit = suit
-        self._face = face
-        self._id = faces[face]+ 10 * suits[suit]
+        # Initialize a playing card object. 
+        self.__Suit = suit
+        self.__Face = face
+        self.face = "Hidden"
+        self.suit = "Hidden"
         self.face_up = face_up
         self.in_hand = in_hand
         self.in_deck = in_deck
+        logging.info(f"The card {self.__Suit:} of {self.__Face:}")
 
-        #print(f"The card {self._suit:} of {self._face:} with a value of {self._id:} has been created")
 
-    def __gt__(self, other):
-        return self._id > other._id 
-
-    def __lt__(self, other):
-        return self._id < other._id
+    def __repr__(self):
+        # Wraps the show method
+        self.show()
 
     def show(self):
-        if self.in_hand is True or self.face_up is True:
-            pass
-            #print(f"{self._id=} (value), {self,_suit=}, {self._face=}")
-        else:
-            raise CardNotViewable("A card must be faced up or in hand to be visible")
+        # Reveals the card if it is face up
+        if self.face_up is False:
+            print("The card is face down and cannot be seen")
+        logging.debug(f"Show card {self.face_up=}, {self.in_hand=}]")
+        return (self.face, self.suit)
     
     def flip(self):
-        pre_val = self.face_up
-        try:
-            self.face_up = True if self.face_up is False else False
-            if self.face_up is not pre_val:
-                return True
-        except DeckException as e:
-            raise(e, "Something is wrong")
-        return False
+        logging.info("Flipping card")
+        if self.face_up is False:
+            self.face = self.__Face
+            self.suit = self.__Suit
+            self.face_up = True
+            logging.info("Revealing card")
+        else:
+            self.face = "Hidden"
+            self.suit = "Hidden"
+            self.face_up = False
+            logging.info("Card hidden")
